70208c6e63550f4a06dfbe6f34cc504f21f41f05
BergdorfK <bergdorf.kolos@gmail.com>
2025-08-05 21:18:10 +0300

    init

---
+++ study-with-gpt/src/Day_1/MyInfo.java
@@ -0,0 +1,23 @@
+package Day_1;
+
+public class MyInfo {
+    public static void main(String[] args) {
+        String name = "Иван";
+        int age = 20;
+        double height = 1.75;
+        boolean isProgrammer = true;
+
+        System.out.println("Меня зовут: " + name);
+        System.out.println("Мне " + age + " лет");
+        System.out.println("Мой рост: " + height + " м");
+        System.out.println("Умею программировать: " + isProgrammer );
+
+        /**
+         * Меня зовут: Иван
+         * Мне 20 лет
+         * Мой рост: 1.75 м
+         * Умею программировать: true
+         */
+    }
+
+}
